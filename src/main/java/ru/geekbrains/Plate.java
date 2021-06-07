a22176b09b69afee16aade51e36fbd69aadc9e7c
AnbuStyle <khumaun99@gmail.com>
2021-06-07 16:13:45 +0300

    предварительное решение

--- src/main/java/ru/geekbrains/Plate.java
+++ src/main/java/ru/geekbrains/Plate.java
@@ -1,4 +1,23 @@
 package ru.geekbrains;
 
 public class Plate {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    boolean decreaseFood(int n) {
+        int current = food - n;
+        if (current < 0) return false;
+
+        food -= n;
+        return true;
+    }
+    void addFood(int food) {
+        this.food += food;
+    }
+
+    void info() {
+        System.out.println("plate: " + food);
+    }
 }
